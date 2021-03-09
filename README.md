55736d1209133dbc0b37c3ec85955fbbc9935db3
Nadir-bnm <nadir.begiyev@gmail.com>
2021-03-09 15:31:46 +0800

    final V.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# fettuchini.github.io
- 
+# fetched
--- script.js
+++ script.js
@@ -1,66 +1,53 @@
-let url = window.location.search;
-let header = document.getElementById('header');
-let search = document.getElementById('header__request');
-let button = document.getElementById('header__button');
-let main = document.getElementById('main');
+let url = window.location.href;
+let loader = document.getElementById('preloader');
+let wrapper = document.getElementById('wrapper');
 let date = document.getElementById('date');
-let img = document.getElementById('main__photo');
-let login = document.getElementById('main__name');
+let img = document.getElementById('photo');
+let login = document.getElementById('name');
 let bio = document.getElementById('bio');
-let footer = document.getElementById('footer');
-let link = document.getElementById('footer__link');
+let link = document.getElementById('link');
+let cover = document.getElementById('cover');
+let user;
 
-function clickButton() {
-  let getName = new Promise((resolve, reject) => {
-    setTimeout(() => search ? resolve(search) : reject('Error, please enter correct address'), 3000)
-  });
-  
-  let newDate = new Date().toLocaleDateString();
-  let getData = new Promise((resolve, reject) => {
-    setTimeout(() => newDate ? resolve(newDate) : reject('Error, please enter correct address'), 3000);
-  });
-  
-
-  Promise.all([getName, getData])
-    .then (([search, newDate]) => github(search, newDate))
-    .catch(err => console.log(err));
-}
-
-
-
-
-function github(newDate, search) {
-fetch(`https://api.github.com/users/${search.value}`)
-.then(res => res.json())
-.then(json => {
-  if (json.login != null) {
-
-    login.value = json.name;
-  
-    link.href = json.html_url;
-  
-    bio.value = json.bio;
-
-    img.src = json.avatar_url;
-
-    date.value = newDate;
-  
-
+let generator = () => {
+  let divider = url.split('=')[1];
+  if (divider) {
+      name = divider;
   } else {
-    login.value = 'No data! Please enter correct address';
-    link.href = 'Данные отсутствуют';
-    bio.value = 'Данные отсутствуют';
+      name = 'Nadir-bnm';
+  }
+  return 'https://api.github.com/users/' + name;
+};
 
-    setTimeout(function() {
-      document.location.reload();
-    }, 5000);
-  }  
-})
-}
+let apiCall = generator();
 
-if (url != '') {
-  search.value = url.split('=')[1];
-  clickButton();
-} 
+let newDate = new Date().toLocaleDateString();
+let getDate = new Promise((resolve, reject) => {
+  setTimeout(() => newDate ? resolve(date.value = newDate) : reject('Error, please enter correct address'), 1500);
+});
 
-button.addEventListener('click',clickButton);
+let refresher = function(obj) {
+  loader.classList.add('hidden');
+  cover.classList.remove('hidden');
+
+  login.value = obj.name;
+  
+  link.href = obj.html_url;
+
+  img.src = obj.avatar_url;
+
+  if (obj.bio != null) {
+    bio.value = obj.bio;
+  } else {
+    bio.value = 'No data available';
+  }
+};
+
+Promise.all([getDate])
+.then(() => fetch(apiCall))
+.then(res => res.json())
+.then(obj => user = Object.assign({}, obj))
+.then(user => refresher(user));
+
+
+
